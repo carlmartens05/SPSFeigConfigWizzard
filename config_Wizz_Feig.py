7e2a1681362707c97fecb2dff9bf5e83b511bde7
carlmartens05 <carlmartens2005@gmail.com>
2026-02-20 21:53:20 +0100

    auto sluittijd menu

--- config_Wizz_Feig.py
+++ config_Wizz_Feig.py
@@ -5,7 +5,8 @@ parameters = []
 
 def wizard():
     """
-    Volledige wizard met terug-optie.
+    Volledige wizard met loop en sub-menu's.
+     - Hoofdmenu: keuze tussen AS, SG, OHD
     """
     while True:  # hoofdloop van wizard
         # --- Hoofdmenu ---
@@ -31,15 +32,58 @@ def wizard():
         else:
             print("Ongeldige invoer, probeer opnieuw.")
 
+# --- Sub-menu's voor specifieke afsluitingen ---
+# - - - slagbomen - - - 
+
 def as_menu():
     """
     Submenu voor slagboom.
     Geeft True terug als parameters zijn toegevoegd, False als gebruiker terug wilt.
     """
     while True:
-        print("Moet nog gebouwd worden, gebruik je verstand!")
+        afsluiting = "as"  # voor duidelijkheid in sub-menu's
+        as_PLC = input("Wordt de slagboom aangestuurd door een PLC  of is deze standalone (PLC/standalone)? (typ 'terug' om terug te gaan) ").lower()
+        if as_PLC == "plc":
+            return as_PLC_menu()  # Ga naar sub-menu PLC keuzes
+        elif as_PLC == "standalone":
+            return as_standalone_menu()  # Ga naar sub-menu standalone keuzes
+        elif as_PLC == "terug":
+            print("Terug naar hoofdmenu...")
+            return False  # terug naar hoofdmenu
+        else:
+            print("Moet nog gebouwd worden, gebruik je verstand!")
+            input("Druk op Enter om af te sluiten.")
+            return False  # terug naar hoofdmenu
+
+def as_PLC_menu():
+    """
+    Submenu voor slagboom met PLC.
+    Geeft True terug als parameters zijn toegevoegd, False als gebruiker terug wilt.
+    """
+    while True:
+        afsluiting = "as"  # voor duidelijkheid in sub-menu's
+        print("Moet nog gebouwd worden, gebruik je verstand! as_PLC_menu")
         input("Druk op Enter om af te sluiten.")
         return False  # terug naar hoofdmenu
+
+def as_standalone_menu():
+    """
+    Submenu voor slagboom met PLC.
+    Geeft True terug als parameters zijn toegevoegd, False als gebruiker terug wilt.
+    """
+    while True:
+        afsluiting = "as"  # voor duidelijkheid in sub-menu's
+        as_auto_sluittijd = input("wil je de autosluittijd aanpassen (y/n)? (typ 'terug' om terug te gaan) ").lower()
+        if as_auto_sluittijd == "y":
+            return auto_sluittijd_menu()  # Ga naar sub-menu autosluittijd keuzes
+            return True  # klaar, doorgaan
+        elif as_auto_sluittijd == "n":
+            return False  # klaar, naar hoofdmenu
+        elif as_auto_sluittijd == "terug":
+            print("Terug naar hoofdmenu...")
+            return False  # terug naar hoofdmenu
+
+# - - - speedgates - - -
 
 def sg_menu():
     """
@@ -47,6 +91,7 @@ def sg_menu():
     Geeft True terug als parameters zijn toegevoegd, False als gebruiker terug wilt.
     """
     while True:
+        afsluiting = "sg"  # voor duidelijkheid in sub-menu's
         sg_PLC = input("Wordt de speedgate aangestuurd door een PLC (y/n)? (typ 'terug' om terug te gaan) ").lower()
         if sg_PLC == "y":
             # Voeg parameters toe
@@ -61,14 +106,21 @@ def sg_menu():
             parameters.append(("0890", "0"))
             return True  # klaar, doorgaan
         elif sg_PLC == "n":
-            print("Moet nog gebouwd worden, gebruik je verstand!")
-            input("Druk op Enter om af te sluiten.")
-            return False
+            as_auto_sluittijd = input("wil je de autosluittijd aanpassen (y/n)? (typ 'terug' om terug te gaan) ").lower()
+            if as_auto_sluittijd == "y":
+                return auto_sluittijd_menu()  # Ga naar sub-menu autosluittijd keuzes
+            elif as_auto_sluittijd == "n":
+                return False  # klaar, naar hoofdmenu
+            elif as_auto_sluittijd == "terug":
+                print("Terug naar hoofdmenu...")
+                return False  # terug naar hoofdmenu
         elif sg_PLC == "terug":
             print("Terug naar hoofdmenu...")
             return False  # terug naar hoofdmenu
         else:
             print("Ongeldige invoer, probeer opnieuw.")
+
+# - - - Overheaddeur - - -
 
 def ohd_menu():
     """
@@ -76,9 +128,40 @@ def ohd_menu():
     Geeft True terug als parameters zijn toegevoegd, False als gebruiker terug wilt.
     """
     while True:
+        afsluiting = "ohd"  # voor duidelijkheid in sub-menu's
         print("Moet nog gebouwd worden, gebruik je verstand!")
         input("Druk op Enter om af te sluiten.")
         return False  # terug naar hoofdmenu
+
+
+# submenu's voor specifieke keuzes
+
+
+def auto_sluittijd_menu():
+    """
+    Submenu voor slagboom auto sluittijden.
+    Geeft True terug als parameters zijn toegevoegd, False als gebruiker terug wilt.
+    """
+    while True:
+        auto_sluittijd_0010 = input("Wat is de gewenste auto sluittijd voor geheel open (p.010) in seconden? gebruik deze parameter voor de slagbomen. (typ 'terug' om terug te gaan) ").lower()
+        if auto_sluittijd_0010.isdigit() and 0 <= int(auto_sluittijd_0010) <= 200:
+            parameters.append(("0010", auto_sluittijd_0010))  # Voeg parameter toe
+        elif auto_sluittijd_0010 == "":
+            print("instelling overslaan, doorgaan naar volgende parameter...")
+
+        auto_sluittijd_0011 = input("Wat is de gewenste auto sluittijd voor half open (p.011) in seconden? te gebruiken voor speedgates (typ 'terug' om terug te gaan) ").lower()
+        if auto_sluittijd_0011.isdigit() and 0 <= int(auto_sluittijd_0011) <= 200:
+            parameters.append(("0011", auto_sluittijd_0011))  # Voeg parameter toe
+        elif auto_sluittijd_0011 == "":
+             print("instelling overslaan, doorgaan naar volgende parameter...")
+
+        auto_sluittijd_0012 = input("Wat is de gewenste auto sluittijd voor geforceerde sluittijd (p.012) in seconden? te gebruiken voor OHD en speedgates (voor slagbomen gebruik P.010)  (typ 'terug' om terug te gaan) ").lower()
+        if auto_sluittijd_0012.isdigit() and 0 <= int(auto_sluittijd_0012) <= 200:
+            parameters.append(("0012", auto_sluittijd_0012))  # Voeg parameter toe
+            return True  # klaar, doorgaan
+        elif auto_sluittijd_0012 == "terug":
+            print("Terug naar hoofdmenu...")
+            return False  # terug naar hoofdmenu
 
 def vraag_bestandsnaam():
     bestandsnaam_input = input(
