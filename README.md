aca86785d5bb099f2bce4c938619ff11dd1366c4
carlmartens05 <carlmartens2005@gmail.com>
2026-03-18 20:51:39 +0100

    - bugfix node id not 8ba but 8b1

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ er is keuze uit de volgende config menu's:
 3. helligbaan regeling                   v0.1        as/sg/ohd
 4. loopsnelheden                         v0.1        ohd
 5. motor instellingen                    v0.1        ohd
-6. node id tbv pxs barrier control       v0.1        as/sg/ohd
+6. node id tbv pxs barrier control       v0.2        as/sg/ohd
 7. onderhouds interval                   v0.1        as/sg/ohd
 8. verkeerslichten                       v0.1        as/sg/ohd
 9. zelftest                              v0.1        as/ohd
--- utilities/menus/submenus/node_id.py
+++ utilities/menus/submenus/node_id.py
@@ -3,7 +3,7 @@ from ...input_helpers import vraag_getal, vraag_ja_nee, vraag_tekst
 from ...parameter_logic import bereken_input_parameter_code
 
 # ======================
-# node id v0.1
+# node id v0.2
 # ======================
 
 
@@ -11,4 +11,4 @@ def node_id_menu(config):
     node_id = vraag_getal(
         "Wat is de Node ID van de besturing? T.B.V. de PXS Feig koppeling (P.8ba) als standaard, gebruik hier het nummer van je afsluiting. (bijv. SG in -> 1, SG uit -> 2). ")
     if node_id is not None:
-        config.sub_parameter.append(("08ba", node_id))
+        config.sub_parameter.append(("08b1", node_id))
