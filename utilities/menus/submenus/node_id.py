d0ed4632cceab5450076e7670c5829b9eeb99270
carlmartens05 <carlmartens2005@gmail.com>
2026-03-23 18:04:13 +0100

    edited input question, not P.8BA but p.8B1 is asked v0.3

--- utilities/menus/submenus/node_id.py
+++ utilities/menus/submenus/node_id.py
@@ -3,12 +3,12 @@ from ...input_helpers import vraag_getal, vraag_ja_nee, vraag_tekst
 from ...parameter_logic import bereken_input_parameter_code
 
 # ======================
-# node id v0.2
+# node id v0.3
 # ======================
 
 
 def node_id_menu(config):
     node_id = vraag_getal(
-        "Wat is de Node ID van de besturing? T.B.V. de PXS Feig koppeling (P.8ba) als standaard, gebruik hier het nummer van je afsluiting. (bijv. SG in -> 1, SG uit -> 2). ")
+        "Wat is de Node ID van de besturing? T.B.V. de PXS Feig koppeling (P.8b1) als standaard, gebruik hier het nummer van je afsluiting. (bijv. SG in -> 1, SG uit -> 2). ")
     if node_id is not None:
         config.sub_parameter.append(("08b1", node_id))
