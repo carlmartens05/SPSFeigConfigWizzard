752f65f8868f13d906eb57d02f6941dd72114a2a
carlmartens05 <carlmartens2005@gmail.com>
2026-03-26 22:37:48 +0100

    - add import to function, not importing all at ones

--- utilities/menus/submenus/submenus.py
+++ utilities/menus/submenus/submenus.py
@@ -1,5 +1,5 @@
 # ============================
-# subemenu's v0.5
+# subemenu's v0.6
 # ============================
 
 # imports
@@ -7,28 +7,6 @@
 from ...input_helpers import *
 from ...parameter_logic import bereken_input_parameter_code
 
-# ======================
-# Submenu's import
-# ======================
-
-# onderhouds intrval
-from .onderhouds_interval import onderhouds_interval_menu
-# auto sluittijd
-from .auto_sluittijd import auto_sluittijd_menu
-# motor instellingen
-from .motor_instellingen import motor_instelling_menu
-# zelftest
-from .zelftest import zelftest_menu
-# verkeerslichten
-from .verkeerslichten import verkeerslichten_menu
-# node id
-from .node_id import node_id_menu
-# heling baan regeling
-from .heling_baan_regeling import heling_baan_regeling_menu
-# loopsnelheden
-from .loopsnelheden import loopsnelheden_OHD_menu
-# BMI
-from .bmi import BMI_menu
 
 # ============================
 # keuzemenu voor subemenu's
@@ -53,12 +31,16 @@ def keuzemenu_menus_as_plc(config, afsluiting):
                 """)
 
         if submenu_keuze == "1":
+            from .zelftest import zelftest_menu
             zelftest_menu(config, afsluiting)
         elif submenu_keuze == "2":
+            from .node_id import node_id_menu
             node_id_menu(config)
         elif submenu_keuze == "3":
+            from .bmi import BMI_menu
             BMI_menu(config)
 #        elif submenu_keuze == "4":
+#            from .onderhouds_interval import onderhouds_interval_menu
 #            onderhouds_interval_menu(config, "as")
         elif submenu_keuze == "klaar":
             return True
@@ -72,15 +54,19 @@ def keuzemenu_menus_as_plc(config, afsluiting):
 
 def alle_menus_as_plc(config, afsluiting):
     if vraag_ja_nee("Zelftest instellen? (y/n) "):
+        from .zelftest import zelftest_menu
         zelftest_menu(config, "as")
 
     if vraag_ja_nee("Node ID instellen voor PXS Feig koppeling? (y/n) "):
+        from .node_id import node_id_menu
         node_id_menu(config)
 
     if vraag_ja_nee("BMI instellen? (y/n) "):
+        from .bmi import BMI_menu
         BMI_menu(config)
 
 #    if vraag_ja_nee("wil je een onderhoudsteller instellen? (y/n) "):
+#            from .onderhouds_interval import onderhouds_interval_menu
 #        onderhouds_interval_menu(config, "as")
     return True
 
@@ -104,18 +90,25 @@ def keuzemenu_menus_as_standalone(config, afsluiting):
                 """)
 
         if submenu_keuze == "1":
+            from .auto_sluittijd import auto_sluittijd_menu
             auto_sluittijd_menu(config, afsluiting)
         elif submenu_keuze == "2":
+            from .zelftest import zelftest_menu
             zelftest_menu(config, afsluiting)
         elif submenu_keuze == "3":
+            from .verkeerslichten import verkeerslichten_menu
             verkeerslichten_menu(config, afsluiting)
         elif submenu_keuze == "4":
+            from .node_id import node_id_menu
             node_id_menu(config)
         elif submenu_keuze == "5":
+            from .heling_baan_regeling import heling_baan_regeling_menu
             heling_baan_regeling_menu(config)
         elif submenu_keuze == "6":
+            from .bmi import BMI_menu
             BMI_menu(config)
         # elif submenu_keuze == "7":
+        #            from .onderhouds_interval import onderhouds_interval_menu
         #    onderhouds_interval_menu(config, "as")
         elif submenu_keuze == "klaar":
             return True
@@ -129,26 +122,33 @@ def keuzemenu_menus_as_standalone(config, afsluiting):
 
 def alle_menus_as_standalone(config, afsluiting):
     if vraag_ja_nee("Zelftest instellen? (y/n)  "):
+        from .zelftest import zelftest_menu
         zelftest_menu(config, "as")
 
     if vraag_ja_nee("Verkeerslichtsturing instellen? (y/n) "):
+        from .verkeerslichten import verkeerslichten_menu
         verkeerslichten_menu(config, "as")
         if vraag_ja_nee("Hellingbaan regeling instellen? (y/n) "):
+            from .heling_baan_regeling import heling_baan_regeling_menu
             heling_baan_regeling_menu(config)
         else:
             if vraag_ja_nee("Autosluittijd aanpassen? (y/n) "):
+                from .auto_sluittijd import auto_sluittijd_menu
                 auto_sluittijd_menu(config, "as")
     else:
         if vraag_ja_nee("Autosluittijd aanpassen? (y/n) "):
             auto_sluittijd_menu(config, "as")
 
     if vraag_ja_nee("Node ID instellen voor PXS Feig koppeling? (y/n) "):
+        from .node_id import node_id_menu
         node_id_menu(config)
 
     if vraag_ja_nee("BMI instellen? (y/n) "):
+        from .bmi import BMI_menu
         BMI_menu(config)
 
     # if vraag_ja_nee("wil je een onderhoudsteller instellen? (y/n) "):
+    #            from .onderhouds_interval import onderhouds_interval_menu
     #    onderhouds_interval_menu(config, "as")
     return True
 
@@ -171,10 +171,13 @@ def keuzemenu_menus_sg_plc(config, afsluiting):
                 """)
 
         if submenu_keuze == "1":
+            from .node_id import node_id_menu
             node_id_menu(config)
         elif submenu_keuze == "2":
+            from .bmi import BMI_menu
             BMI_menu(config)
         #    elif submenu_keuze == "3":
+        #    from .onderhouds_interval import onderhouds_interval_menu
         #        onderhouds_interval_menu(config, "adv")
         elif submenu_keuze == "klaar":
             return True
@@ -188,11 +191,14 @@ def keuzemenu_menus_sg_plc(config, afsluiting):
 
 def alle_menus_sg_plc(config, afsluiting):
     if vraag_ja_nee("Node ID instellen voor PXS Feig koppeling? (y/n) "):
+        from .node_id import node_id_menu
         node_id_menu(config)
     if vraag_ja_nee("BMI instellen? (y/n) "):
+        from .bmi import BMI_menu
         BMI_menu(config)
 
     # if vraag_ja_nee("wil je een onderhoudsteller instellen? (y/n) "):
+    #    from .onderhouds_interval import onderhouds_interval_menu
     #    onderhouds_interval_menu(config, "sg")
     return True
 
@@ -214,14 +220,19 @@ def keuzemenu_menus_sg_standalone(config, afsluiting):
                 """)
 
         if submenu_keuze == "1":
+            from .auto_sluittijd import auto_sluittijd_menu
             auto_sluittijd_menu(config, afsluiting)
         elif submenu_keuze == "2":
+            from .node_id import node_id_menu
             node_id_menu(config)
         elif submenu_keuze == "3":
+            from .heling_baan_regeling import heling_baan_regeling_menu
             heling_baan_regeling_menu(config)
         elif submenu_keuze == "4":
+            from .bmi import BMI_menu
             BMI_menu(config)
         # elif submenu_keuze == "5":
+        #    from .onderhouds_interval import onderhouds_interval_menu
         #    onderhouds_interval_menu(config, "sg")
         elif submenu_keuze == "klaar":
             return True
@@ -235,23 +246,32 @@ def keuzemenu_menus_sg_standalone(config, afsluiting):
 
 def alle_menus_sg_standalone(config, afsluiting):
     if vraag_ja_nee("Hellingbaan regeling instellen? (y/n) "):
+        from .heling_baan_regeling import heling_baan_regeling_menu
         heling_baan_regeling_menu(config)
     else:
         if vraag_ja_nee("Autosluittijd aanpassen? (y/n) "):
+            from .auto_sluittijd import auto_sluittijd_menu
             auto_sluittijd_menu(config, "sg")
 
     if vraag_ja_nee("Node ID instellen voor PXS Feig koppeling? (y/n) "):
+        from .node_id import node_id_menu
         node_id_menu(config)
 
     if vraag_ja_nee("BMI instellen? (y/n) "):
+        from .bmi import BMI_menu
         BMI_menu(config)
 
     # if vraag_ja_nee("wil je een onderhoudsteller instellen? (y/n) "):
+    #    from .onderhouds_interval import onderhouds_interval_menu
     #    onderhouds_interval_menu(config, "sg")
     return True
 
+# == overheaddeur ==
 
-def keuzemenu_menus_ohd(config, afsluiting):
+# === ohd standalone ===
+
+
+def keuzemenu_menus_ohd_standalone(config, afsluiting):
     while True:
         submenu_keuze = vraag_tekst("""
                 ===== welk submenu wil je doorlopen? ===== 
@@ -264,32 +284,45 @@ def keuzemenu_menus_ohd(config, afsluiting):
                 7 = BMI instellingen
                 8 = loopsnelheden instellen
                 9 = boost instellingen 
+                10 = positioneringssysteem
                 klaar = klaar met configureren en maak bestand aan. 
                 terug = terug naar adv. menu. 
                 (enter = terug naar hoofdmenu)
                 """)
 
         if submenu_keuze == "1":
+            from .auto_sluittijd import auto_sluittijd_menu
             auto_sluittijd_menu(config, afsluiting)
         elif submenu_keuze == "2":
+            from .motor_instellingen import motor_instelling_menu
             motor_instelling_menu(config)
         elif submenu_keuze == "3":
+            from .zelftest import zelftest_menu
             zelftest_menu(config, afsluiting)
         elif submenu_keuze == "4":
+            from .verkeerslichten import verkeerslichten_menu
             verkeerslichten_menu(config, afsluiting)
         elif submenu_keuze == "5":
+            from .node_id import node_id_menu
             node_id_menu(config)
         elif submenu_keuze == "6":
+            from .heling_baan_regeling import heling_baan_regeling_menu
             heling_baan_regeling_menu(config)
         elif submenu_keuze == "7":
+            from .bmi import BMI_menu
             BMI_menu(config)
         elif submenu_keuze == "8":
+            from .loopsnelheden import loopsnelheden_OHD_menu
             loopsnelheden_OHD_menu(config)
         # elif submenu_keuze == "9":
+        #    from .onderhouds_interval import onderhouds_interval_menu
         #    onderhouds_interval_menu(config, "adv")
         elif submenu_keuze == "9":
             from submenus.boost import boost_menu
             boost_menu(config)
+        elif submenu_keuze == "10":
+            from .positioning_system_profile import positioning_system_profile
+            positioning_system_profile(config)
         elif submenu_keuze == "klaar":
             return True
         elif submenu_keuze == "terug":
@@ -301,31 +334,47 @@ def keuzemenu_menus_ohd(config, afsluiting):
             print("Ongeldige keuze.")
 
 
-def alle_menus_ohd(config, afsluiting):
+def alle_menus_ohd_standalone(config, afsluiting):
     if vraag_ja_nee("Motor instellingen aanpassen? (y/n) "):
+        from .motor_instellingen import motor_instelling_menu
         motor_instelling_menu(config)
 
-    if vraag_ja_nee("loopsnelheden aanpassen (y/n)"):
+    if vraag_ja_nee("positioneringssysteem instellen? (y/n)   "):
+        from .positioning_system_profile import positioning_system_profile
+        positioning_system_profile(config)
+
+    if vraag_ja_nee("loopsnelheden aanpassen (y/n)  "):
+        from .loopsnelheden import loopsnelheden_OHD_menu
         loopsnelheden_OHD_menu(config)
 
+    if vraag_ja_nee("boost intellen? (y/n)   "):
+        from .boost import boost_menu
+        boost_menu(config)
+
     if vraag_ja_nee("Verkeerslichtsturing instellen? (y/n) "):
+        from .verkeerslichten import verkeerslichten_menu
         verkeerslichten_menu(config, "ohd")
         if vraag_ja_nee("Hellingbaan regeling instellen? (y/n) "):
+            from .heling_baan_regeling import heling_baan_regeling_menu
             heling_baan_regeling_menu(config)
         else:
             if vraag_ja_nee("Autosluittijd aanpassen? (y/n) "):
+                from .auto_sluittijd import auto_sluittijd_menu
                 auto_sluittijd_menu(config, "ohd")
     else:
         if vraag_ja_nee("Autosluittijd aanpassen? (y/n) "):
             auto_sluittijd_menu(config, "ohd")
 
     if vraag_ja_nee("Node ID instellen voor PXS Feig koppeling? (y/n) "):
+        from .node_id import node_id_menu
         node_id_menu(config)
 
     if vraag_ja_nee("BMI instellen? (y/n) "):
+        from .bmi import BMI_menu
         BMI_menu(config)
 
     # if vraag_ja_nee("wil je een onderhoudsteller instellen? (y/n) "):
+    #    from .onderhouds_interval import onderhouds_interval_menu
     #    onderhouds_interval_menu(config, "ohd")
 
     return True
