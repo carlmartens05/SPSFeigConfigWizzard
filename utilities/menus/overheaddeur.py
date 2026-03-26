2d4294470a896f72cea5ff90e7e5feac2b5b0815
carlmartens05 <carlmartens2005@gmail.com>
2026-03-26 22:45:33 +0100

    - added PLC and standalone to OHD

--- utilities/menus/overheaddeur.py
+++ utilities/menus/overheaddeur.py
@@ -5,13 +5,25 @@ from .submenus.submenus import *
 
 
 # ======================
-# Overheaddeuren v0.2
+# Overheaddeuren v0.3
 # ======================
 
 
 def ohd_menu(config):
-    if vraag_ja_nee("wil je alle menu's doorlopen of wil je kiezen welke menu's ingesteld moeten worden? kiezen = y, alles = n "):
-        keuzemenu_menus_ohd(config, "ohd")
-    else:
-        alle_menus_ohd(config, "ohd")
+    ohd_plc = input(
+        "Wordt de speedgate aangestuurd door een PLC (y/n)? plc maar geen open/dicht/stop? kies nee  ")
+    if ohd_plc == "n":
+        if vraag_ja_nee("wil je alle menu's doorlopen of wil je kiezen welke menu's ingesteld moeten worden? kiezen = y, alles = n "):
+            keuzemenu_menus_ohd_standalone(config, "ohd")
+        else:
+            alle_menus_ohd_standalone(config, "ohd")
+    if ohd_plc == "y":
+        ohd_plc_menu(config, "ohd")
     return True
+
+
+def ohd_plc_menu(config):
+    config.hoofd_parameter.extend(
+        [("0501", "0101"), ("0502", "0401"), ("0503", "0701"), ("0505", "1403"), ("0701", "0101"), ("0702", "0201")])
+    config.sub_parameter.extend(
+        [("0522", "1"), ("0010", "0"), ("0011", "0"), ("0012", "0")])
