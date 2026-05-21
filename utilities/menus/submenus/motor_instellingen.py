d6ced9db0887d5a48fb31831cdb3af480b99c5ca
Carl Martens <carlmartens2005@gmail.com>
2026-05-21 18:35:47 +0200

    added GfA SI 25.10 to motorprofiles

--- utilities/menus/submenus/motor_instellingen.py
+++ utilities/menus/submenus/motor_instellingen.py
@@ -21,6 +21,7 @@ def motor_instelling_menu(config):
                 ===== wil je een motor profiel selecteren? ( controleer altijd zelf of de ingestelde waarde klopt!!!) =====
                 0 = alles zelf instellen
                 1 = GfA SE 9.24  (part no. 10002188 )
+                2 = GfA SI 25.10 (part no. 10003166 )
                 """)
     if motor_profile == "0":
         freq = vraag_getal("Frequentie motor (P.100 Hz)")
@@ -46,6 +47,13 @@ def motor_instelling_menu(config):
         config.sub_parameter.append(("0102", "60"))
         config.sub_parameter.append(("0103", "230"))
         print("standaard parameters toegevoegd voor de GfA SE 9.24 (part no. 10002188 ) ")
+
+    if motor_profile == "1":
+        config.sub_parameter.append(("0100", "50"))
+        config.sub_parameter.append(("0101", "31"))
+        config.sub_parameter.append(("0102", "51"))
+        config.sub_parameter.append(("0103", "230"))
+        print("standaard parameters toegevoegd voor de GfA SI 25.10 (part no. 10003166 ) ")
 
     # Boost alleen vragen als nog niet ingesteld
     if not config.boost_ingesteld() and vraag_ja_nee("wil je een boost instellen? (y/n)"):
